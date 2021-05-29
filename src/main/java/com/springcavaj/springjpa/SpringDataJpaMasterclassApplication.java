e25c13d0534c3e556c0c8bc17298733ad9245c05
c86amik <amik.c17@gmail.com>
2021-05-29 12:02:16 +0530

    Initial Commit

---
+++ src/main/java/com/springcavaj/springjpa/SpringDataJpaMasterclassApplication.java
@@ -0,0 +1,13 @@
+package com.springcavaj.springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMasterclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaMasterclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcavaj/springjpa/SpringDataJpaMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcavaj.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
